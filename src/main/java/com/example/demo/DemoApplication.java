5696139be274df86b1bf8b0b5159b0ce4dd5ceac
jaeoan <wnsxo104@gmail.com>
2026-08-24 12:51:20 +0900

    chore: 미사용 HelloController 제거

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("하이");
-	}
-
-}
-
-@RestController
-class HelloController {
-
-	@GetMapping("/")
-	public String hello() {
-		return "하이";
 	}
 
 }
